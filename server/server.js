e3441ca5b71eb9a1aa015daeb620defcbc700369
Matt Moua <matmoua@gmail.com>
2022-11-30 14:52:05 -0600

    Removed unused server routes

--- server/server.js
+++ server/server.js
@@ -9,7 +9,6 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
-const rainforestRouter = require('./routes/rainforest.router')
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -24,7 +23,6 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
-app.use('/api/rainforest', rainforestRouter);
 
 // Serve static files
 app.use(express.static('build'));
